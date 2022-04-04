2128040abd6f2cb6a6e06428f4ad59e9f7d2e3bf
vpham26 <vulong.pham26@gmail.com>
2022-04-03 22:42:46 -0400

    modified package.json and server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const session = require("express-session");
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({
-    helpers
-});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const session = require('express-session');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -23,22 +18,29 @@ const sess = {
         db: sequelize,
         checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
         expiration: 1000 * 60 * 30 // will expire after 30 minutes
-    })
+    }),
 };
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({
+    helpers
+});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: true
+    extended: false
 }));
-app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync();
+app.use(require("./controllers"));
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => 
+    console.log(`App listening on port ${PORT}!`));
 });
